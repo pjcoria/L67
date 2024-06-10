b24782dbf612d0f6a77a07e5e85ab22b4eec55e7
Pablo Coria Ledo <pablocoria2000@gmail.com>
2024-06-09 23:49:49 -0300

    Mejora del codigo

--- data_plots.py
+++ data_plots.py
@@ -2,43 +2,57 @@
 import matplotlib.pyplot as plt
 import numpy as np
 import os
+from scipy.optimize import curve_fit
 #%% Cargamos los datos
+
 path = 'D:\\Exactas\\Laboratorio_VI_VII\\L67\\Mediciones_fotodiodo\\650nm'
-
-data_dict = {'650nm-0,7uW-OD5,2.csv': {'data' :[[],[]]}, '650nm-1,2uW-OD4,9.csv': {'data' :[[],[]]}, '650nm-1,3uW-OD4,7.csv': {'data' :[[],[]]},
-           '650nm-1,8uW-OD4,5.csv': {'data' :[[],[]]}, '650nm-4,6uW-OD4,0.csv': {'data' :[[],[]]}, '650nm-5,0uW-OD3,8.csv': {'data' :[[],[]]},
-           '650nm-6,2uW-OD3,7.csv': {'data' :[[],[]]}, '650nm-7,1uW-OD3,6.csv': {'data' :[[],[]]}, '650nm-7,3uW-OD3,5.csv': {'data' :[[],[]]},
-           '650nm-8,6uW-OD3,4.csv': {'data' :[[],[]]}}
-
-for file in data_dict.keys():
-    data_dict[file]['data'][0], data_dict[file]['data'][1] = np.loadtxt(path+ '\\' +file, skiprows=1, unpack=True)
+ext = '.csv'
+txt_files = [i for i in os.listdir(path) if os.path.splitext(i)[1] == ext]
+data_dict={}
+for file in txt_files:
+    data_dict[file] = {'data' :[[],[]]}
+    data_dict[file]['data'][0], data_dict[file]['data'][1] = np.loadtxt(path + '\\' + file, skiprows=1, unpack=True)
     data_dict[file]['data'][0] = np.array(data_dict[file]['data'][0])
     data_dict[file]['data'][1] = np.array(data_dict[file]['data'][1])
 
-#%% Graficamos todas las series de datos
+
+#%% Graficamos todas las series de datos y calculamos la tensión maxima registrada en promedio
+
+maxs=[]
+pots=[]
 
 for idx, file in enumerate (data_dict.keys()):
+    ymax = data_dict[file]['data'][1][40:].max()
+    #ymax_index = np.where(data_dict[file]['data'][1] == ymax)[0][0]
+    ymax_index_min = np.where(2.0<(data_dict[file]['data'][0]*1000000))[0]
+    ymax_index_max = np.where(data_dict[file]['data'][0]*1000000<3)[0]
+    y_range = np.intersect1d(ymax_index_min, ymax_index_max, assume_unique=False, return_indices=False)
+    ymax_mean = data_dict[file]['data'][1][y_range[0]:y_range[-1]].mean()
+    maxs.append(ymax_mean)
+    pots.append(float(file[6:9].replace(',', '.')))
     plt.Figure()
     plt.title(file)
     plt.plot(data_dict[file]['data'][0][40:]*1000000, data_dict[file]['data'][1][40:]*1000)
+    plt.plot(data_dict[file]['data'][0][y_range[0]:y_range[-1]]*1000000, data_dict[file]['data'][1][y_range[0]:y_range[-1]]*1000, color='r')
     plt.xlabel('Tiempo [us]')
     plt.ylabel('Tension [mV]')
     plt.grid()
     plt.show()
 
-
 #%% Calculamos y graficamos la tension maxima registrada en funcion de la potencia del laser
 
-maxs=[]
-pots=[]
+def f(x,a,b):
+    return a*x+b
 
-for file in data_dict.keys():
-    ymax = data_dict[file]['data'][1][40:].max()
-    ymax_index = np.where(data_dict[file]['data'][1]==ymax)[0][0]
-    ymax_mean = data_dict[file]['data'][1][ymax_index-20:ymax_index+20].mean()
-    maxs.append(ymax_mean)
-    pots.append(float(file[6:9].replace(',','.')))
+popt, pcov = curve_fit(f, pots, maxs)
+x = np.linspace(pots[0],pots[-1],100)
 
 plt.figure()
+plt.title('Voltaje maximo adquirido en funcion de la potencia recibida')
 plt.scatter(pots,maxs)
+plt.grid()
+plt.xlabel('Potencia [uW]')
+plt.ylabel('Voltaje maximo promedio [mV]')
+plt.plot(x,f(x,*popt), linestyle='--', color='r', label=f'Ajuste lineal, a={round(popt[0],4)}, b={round(popt[1],4)}')
+plt.legend()
 plt.show()
